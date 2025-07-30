57531558298b7a28c420ef36db1c791ddadc6d44
nix-config-storage <190307147+nix-config-storage@users.noreply.github.com>
2025-07-30 19:07:33 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,43 +1,66 @@
 {
-  description = "A very basic flake";
+  description = "well... I guess fuck everything I was doing, this is way better";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-flake-parts.url = "github:hercules-ci/flake-parts";
-files.url = "github:mightyiam/files";
-
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, files ... }:
-  # https://flake.parts/module-arguments.html
-  flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
-    imports = [
-      # Optional: use external flake logic, e.g.
-      # inputs.foo.flakeModules.default
- inputs.files.flakeModules.default
-./files-example.nix
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ flake-parts, devenv-root, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" "i686-linux"  ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages.default = pkgs.hello;
+
+        devenv.shells.default = {
+          name = "my-project";
+
+          imports = [
+            # This is just like the imports in devenv.nix.
+            # See https://devenv.sh/guides/using-with-flake-parts/#import-a-devenv-module
+            # ./devenv-foo.nix
+          ];
+
+          # https://devenv.sh/reference/options/
+          packages = [ config.packages.default 
+
 ];
-    flake = {
-      # Put your original flake attributes here.
+
+          enterShell = ''
+            hello
+          '';
+
+          processes.hello.exec = "hello";
+        };
+
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
     };
-    systems = [
-      # systems for which you want to build the `perSystem` attributes
-      "x86_64-linux"
-      # ...
-    ];
-    perSystem = { config, pkgs, ... }: {
-      # Recommended: move all package definitions here.
-      # e.g. (assuming you have a nixpkgs input)
-      # packages.foo = pkgs.callPackage ./foo/package.nix { };
-      # packages.bar = pkgs.callPackage ./bar/package.nix {
-      #   foo = config.packages.foo;
-      # };
-    };
-  });
-
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.default;
-  };
 }
